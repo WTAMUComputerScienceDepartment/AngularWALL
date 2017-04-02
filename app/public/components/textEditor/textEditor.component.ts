c2b665e492ad516dd68d0ead53df7582cb55cbb0
KhainTCore <danceofthelotas@yahoo.co.uk>
2017-04-01 20:48:17 -0700

    Text Editor Template
    Added the template for the text editor once we add Ace Editor.

---
+++ app/public/components/textEditor/textEditor.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+// import * as ace from "ace-builds";
+
+@Component({
+  selector: "text-editor",
+  templateUrl: "app/public/components/textEditor/textEditor.component.html",
+  providers: [],
+  styleUrls: ["app/public/components/textEditor/textEditor.component.css"]
+})
+
+export class TextEditorComponent {
+
+};
